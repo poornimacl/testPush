656619ddb0548235a38a753f66384e4995c3d73a
unknown <cholenp@USVHCHOLENP.global.baxter.com>
2014-12-08 09:13:28 -0600

    update

--- js/index.js
+++ js/index.js
@@ -36,9 +36,19 @@ var app = {
         app.receivedEvent('deviceready');
         alert('Device is ready!');
         console.log('Device  ready');
-        var pushNotification = window.plugins.pushNotification;
+          alert('Opening URL !!');
+                    var ref = window.open('http://192.168.1.12:8100', '_blank', 'location=yes ,toolbar=yes, EnableViewPortScale=yes');
+                    ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+                    ref.addEventListener('loadstop', function() {
+                       ref.executeScript({ code: "var x = document.getElementById('platform').value = 'Google'" });
+                       ref.executeScript({ code: "var x = document.getElementById('token').value = 'Testme'" }); 
+                        showalert();          
+                       });
+                  ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+                  ref.addEventListener('exit', function(event) { alert(event.type); });   
+    //    var pushNotification = window.plugins.pushNotification;
       
-        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"211518520885","ecb":"app.onNotificationGCM"});
+    //    pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"211518520885","ecb":"app.onNotificationGCM"});
         
         
 
